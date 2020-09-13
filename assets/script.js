826b269625191def3e2a4a9074abfc947bd9733b
Andrew Siegel <siegelal7@gmail.com>
2020-09-13 12:44:33 -0400

    added icons and got the lon/lat to work for the 1call api call

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,12 @@ $(document).ready(function () {
   var uv = $("#uv");
   var today = $("#city-date");
   var momentDate = moment().format("M/D/YYYY");
+  var tmrw = moment().add(1, "days").format("M/D/YYYY");
+  var TwoDays = moment().add(2, "days").format("M/D/YYYY");
+  var ThreeDays = moment().add(3, "days").format("M/D/YYYY");
+  var FourDays = moment().add(4, "days").format("M/D/YYYY");
+  var FiveDays = moment().add(5, "days").format("M/D/YYYY");
+  // console.log(tmrw);
   var dayOneTemp = $("#day-one-temp");
   var dayTwoTemp = $("#day-two-temp");
   var dayThreeTemp = $("#day-three-temp");
@@ -25,6 +31,11 @@ $(document).ready(function () {
   var dayThreeDate = $("#day-three-date");
   var dayFourDate = $("#day-four-date");
   var dayFiveDate = $("#day-five-date");
+  var dayOneIcon = $("#day-one-icon");
+  var dayTwoIcon = $("#day-two-icon");
+  var dayThreeIcon = $("#day-three-icon");
+  var dayFourIcon = $("#day-four-icon");
+  var dayFiveIcon = $("#day-five-icon");
   // var urlQueryTwo = `https://api.openweathermap.org/data/2.5/forecast?q=${val}&appid=a573ba9eea9e8c716df06e0ed9a541d6&units=imperial`;
   // $.ajax({
   //   url: urlQueryTwo,
@@ -50,13 +61,13 @@ $(document).ready(function () {
     searchHistory.push(storeObject);
     localStorage.setItem("city", JSON.stringify(searchHistory));
     //basic weather info
-    var nextUrl = "";
+
     $.ajax({
       url: urlQuery,
       method: "GET",
     }).then(function (response) {
       // console.log(response);
-      temp.text(`Temperature: ${response.main.temp}˚`);
+      temp.text(`Temperature: ${response.main.temp}˚F`);
       wind.text(`Wind Speed: ${response.wind.speed} MPH`);
       today.text(`${val} (${momentDate})`);
       humidity.text(`${response.main.humidity}%`);
@@ -64,33 +75,50 @@ $(document).ready(function () {
       var lat = response.coord.lat;
       // console.log(lon);
       // console.log(lat);
-      nextUrl =
-        nextUrl +
-        `http://api.openweathermap.org/data/2.5/uvi?appid=a573ba9eea9e8c716df06e0ed9a541d6&lat=${lat}&lon=${lon}`;
-    });
 
-    console.log(nextUrl);
+      var nextUrl2 = `https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely&appid=a573ba9eea9e8c716df06e0ed9a541d6&lat=${lat}&lon=${lon}&units=imperial`;
 
-    $.ajax({
-      url: urlTwo,
-      method: "GET",
-    }).then(function (r) {
-      console.log(r);
-      dayOneTemp.text(r.list[3].main.temp);
-      dayOneHum.text(r.list[3].main.humidity);
-      dayTwoTemp.text(r.list[11].main.temp);
-      dayTwoHum.text(r.list[11].main.humidity);
-      dayThreeTemp.text(r.list[19].main.temp);
-      dayThreeHum.text(r.list[19].main.humidity);
-      dayFourTemp.text(r.list[27].main.temp);
-      dayFourHum.text(r.list[27].main.humidity);
-      dayFiveTemp.text(r.list[35].main.temp);
-      dayFiveHum.text(r.list[35].main.humidity);
-      dayOneDate.text(r.list[3].dt_txt.substring(0, 10));
-      dayTwoDate.text(r.list[11].dt_txt.substring(0, 10));
-      dayThreeDate.text(r.list[19].dt_txt.substring(0, 10));
-      dayFourDate.text(r.list[27].dt_txt.substring(0, 10));
-      dayFiveDate.text(r.list[35].dt_txt.substring(0, 10));
+      //   $.ajax({
+      //     url: nextUrl,
+      //     method: "GET",
+      //   }).then(function (resp) {
+      //     // console.log(resp);
+      //     uv.text(`UV Index: ${resp.value}`);
+      //   });
+      // });
+
+      $.ajax({
+        url: nextUrl2,
+        method: "GET",
+      }).then(function (resp) {
+        var iconOne = `http://openweathermap.org/img/wn/${resp.daily[0].weather[0].icon}@2x.png`;
+        var iconTwo = `http://openweathermap.org/img/wn/${resp.daily[1].weather[0].icon}@2x.png`;
+        var iconThree = `http://openweathermap.org/img/wn/${resp.daily[2].weather[0].icon}@2x.png`;
+        var iconFour = `http://openweathermap.org/img/wn/${resp.daily[3].weather[0].icon}@2x.png`;
+        var iconFive = `http://openweathermap.org/img/wn/${resp.daily[4].weather[0].icon}@2x.png`;
+        console.log(resp);
+        uv.text(`UV Index: ${resp.current.uvi}`);
+        dayOneTemp.text(`Temp: ${resp.daily[1].temp.day}˚F`);
+        dayOneHum.text(`Humidity: ${resp.daily[1].humidity}%`);
+        dayTwoTemp.text(`Temp: ${resp.daily[2].temp.day}˚F`);
+        dayTwoHum.text(`Humidity: ${resp.daily[2].humidity}%`);
+        dayThreeTemp.text(`Temp: ${resp.daily[3].temp.day}˚F`);
+        dayThreeHum.text(`Humidity: ${resp.daily[3].humidity}%`);
+        dayFourTemp.text(`Temp: ${resp.daily[4].temp.day}˚F`);
+        dayFourHum.text(`Humidity: ${resp.daily[4].humidity}%`);
+        dayFiveTemp.text(`Temp: ${resp.daily[5].temp.day}˚F`);
+        dayFiveHum.text(`Humidity: ${resp.daily[5].humidity}%`);
+        dayOneDate.text(tmrw);
+        dayTwoDate.text(TwoDays);
+        dayThreeDate.text(ThreeDays);
+        dayFourDate.text(FourDays);
+        dayFiveDate.text(FiveDays);
+        dayOneIcon.attr("src", iconOne);
+        dayTwoIcon.attr("src", iconTwo);
+        dayThreeIcon.attr("src", iconThree);
+        dayFourIcon.attr("src", iconFour);
+        dayFiveIcon.attr("src", iconFive);
+      });
     });
   });
 
@@ -102,12 +130,4 @@ $(document).ready(function () {
   //     console.log(response);
   //   });
   // }
-
-  // var saveObject = {
-  //     //finds the value of each of the sibling elements in the generated html-set them as values in object to push to array then push to localStorage
-  //     time: $(this).siblings("div").text(),
-  //     task: $(this).siblings("textarea").val(),
-  //   };
-  //   wholeList.push(saveObject);
-  //   localStorage.setItem("todoList", JSON.stringify(wholeList));
 });
